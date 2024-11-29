e9a0e69e916266d4f9eba9b4d62ac4976ef9ba3a
Gabriel Oliveira <gabriel26012007@gmail.com>
2024-11-29 11:34:56 -0300

    arrumando a cagada

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict'
 const switcher =document.querySelector('.btn')
 switcher.addEventListener('click', function(){
-    document.body.classList.toggle('Roxo-theme')
+    document.body.classList.toggle('dark-theme')
     var className = document.body.className;
-    if(className == 'Azul-theme'){
-        this.textContent = "Roxo";
+    if(className == 'light-theme'){
+        this.textContent = "dark";
     }
     else{
-        this.textContent = "Azul";
+        this.textContent = "light";
     }
     console.log('current class name: '+ className);
 });
